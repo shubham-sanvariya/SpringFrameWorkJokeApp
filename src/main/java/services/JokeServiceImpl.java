7cb529ab82a6af48ab5cb7bc1ccb8d48975369de
shubham-sanvariya <shubhamsanvariyait.com>
2023-01-03 23:45:19 +0530

    created JokeService and its implementation

---
+++ src/main/java/services/JokeServiceImpl.java
@@ -0,0 +1,19 @@
+package services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
